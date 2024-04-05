d023471577690a766be256f76949c1111150c1be
Potyomko Veronika <vnpotomko@gmail.com>
2024-04-05 19:15:07 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,12 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, RouterProvider } from 'react-router-dom';
 import { router } from 'router/router';
 
-ReactDOM.createRoot(document.getElementById('root')).render( <
-    React.StrictMode >
-    <
-    Provider store = { store } >
-    <
-    RouterProvider router = { router } >
-    <
-    App / >
-    <
-    /RouterProvider> <
-    /Provider> <
-    /React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render( 
+  <React.StrictMode>
+      <Provider store={store}>
+        <RouterProvider router={router}>
+        <App />
+        </RouterProvider>
+      </Provider>
+  </React.StrictMode>
 );
