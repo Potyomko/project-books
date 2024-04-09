18f9a565e1a0820b67fc8a97339c49efaf9f7938
Potyomko Veronika <vnpotomko@gmail.com>
2024-04-09 19:37:58 +0300

    add basic styles

---
+++ src/components/Theme.jsx
@@ -0,0 +1,42 @@
+export const Theme = {
+    colors: {
+    black: '#000000',
+    veryDarkBlue: '#242A37',
+    white: '#fff',
+    whiteBackground: '#E0E5EB',
+    greyBackground: '#F6F7FB',
+    orange: '#FF6B08',
+    grey: '#F6F7FB',
+  },
+  space: [0, 2, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    body: 'Montserrat',
+    heading: 'Abril Fatface',
+    google: 'Roboto',
+  },
+  fontSizes: {
+    xxs: '10px',
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    l: '34px',
+  },
+    fontWeights: {
+      extraLight: 200,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      bold: 600,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+  },
+  radii: {
+    none: '0',
+    sm: '4px',
+    md: '8px',
+    lg: '16px',
+    round: '50%',
+  },
+};
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import { router } from 'router/router';
 
 ReactDOM.createRoot(document.getElementById('root')).render( 
