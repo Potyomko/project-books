61d2c0fb5e7a50f02d3e5e2189335fb0cd18a63a
Potyomko Veronika <vnpotomko@gmail.com>
2024-04-13 13:24:00 +0300

    add basic files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 // import { useSelector } from "react-redux";
 // import { selectIsLoggedIn } from "redux/auth/selectors";
 import { Outlet } from "react-router-dom";
+// import { Training } from "./Training/Training";
 
 export const App = () => {
   // const isLoggedIn = useSelector(selectIsLoggedIn)
@@ -9,18 +10,7 @@ export const App = () => {
 {/*     
      {isLoggedIn ? <UserMenu/> :  <AuthMenu/> } */}
     <Outlet/> 
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+     {/* <Training/> */}
     </>
     
   );
---
+++ src/components/Training/TrainingFilter.jsx
@@ -0,0 +1,12 @@
+export const TrainingFilter = () => {
+    <div>
+        <div>
+            <select>Початок</select>
+            <select>Закінчення</select>
+        </div>
+        <div>
+            <select>Обрати книги з бібліотеки</select>
+            <button>Додати</button>
+        </div>
+    </div>
+}
