96d69c46db7acb2243775e115c591c478c447f97
Sophia <105670711+Yorabest@users.noreply.github.com>
2024-04-20 17:47:02 +0300

    add pages form

---
+++ src/components/Statistics/components/addPages.jsx
@@ -0,0 +1,43 @@
+// import {Select} from '../style/hidenList.styled'
+import {useState} from "react"
+import {useSelector} from 'react-redux'
+import { selectFinishDate } from '../../../redux/traning/selectors';
+
+export const AddPages = () => {
+    const [dateValue, setDateValue] = useState('')
+    const [pages, setPages] = useState('')
+    const timeOfTheFinish = useSelector(selectFinishDate)
+    const timeOfTheStart = useSelector(state => state.traning.startDate)
+
+    const handleStartDateChange = (e) => {
+      const chosenDate = Date.parse(e.target.value)/1000
+      if (chosenDate >= timeOfTheStart && chosenDate <= timeOfTheFinish){
+        setDateValue(e.target.value)
+      }
+    };
+
+    const handleInputChange = (e) => {
+        setPages(e.target.value)
+    }
+    
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+         console.log(dateValue, pages)
+    }
+    return <form onSubmit={handleFormSubmit}>
+      <input
+          value={dateValue}
+          type="date"
+          placeholder="Початок"
+          onChange={handleStartDateChange}
+        />
+        <input 
+        value={pages}
+        type="number"
+        // placeholder="Початок"
+        onChange={handleInputChange}
+        />
+        <button type="submit">Submit</button>
+    
+  </form>
+}
---
+++ src/components/Statistics/components/selectDate.jsx
@@ -0,0 +1,27 @@
+import {Select} from '../style/hidenList.styled'
+import {useState} from "react"
+import {useSelector} from 'react-redux'
+import { selectFinishDate } from '../../../redux/traning/selectors';
+
+export const SelectDate = () => {
+    const [dateValue, setDateValue] = useState('')
+    const timeOfTheFinish = useSelector(selectFinishDate)
+    const timeOfTheStart = useSelector(state => state.traning.startDate)
+
+    const handleStartDateChange = (e) => {
+      const chosenDate = Date.parse(e.target.value)/1000
+      if (chosenDate >= timeOfTheStart && chosenDate <= timeOfTheFinish){
+        setDateValue(e.target.value)
+      }
+    };
+    
+    return <Select>
+      <input
+          value={dateValue}
+          type="date"
+          placeholder="Початок"
+          onChange={handleStartDateChange}
+        />
+    
+  </Select>
+}
